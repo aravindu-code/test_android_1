e6687e68f24a909c6a346966bab33e6befa1596d
aravindu-code <53659557+aravindu-code@users.noreply.github.com>
2019-08-30 10:46:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/test_android_1/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.test_android_1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.hardware.SensorManager;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorEvent;
+import android.hardware.Sensor;
+import java.util.List;
+import java.util.Random;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+
+public class MainActivity extends Activity {
+    SensorManager sm = null;
+    TextView textView1 = null;
+    List list;
+
+    private static final Random RANDOM = new Random();
+    private LineGraphSeries<DataPoint> series;
+    private int lastX = 0;
+
+     SensorEventListener sel = new SensorEventListener(){
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+        public void onSensorChanged(SensorEvent event) {
+            float[] values = event.values;
+            textView1.setText("X: "+values[0]+"\nY: "+values[1]+"\nZ: "+values[2]);
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* Get a SensorManager instance */
+        sm = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        textView1 = (TextView)findViewById(R.id.textView);
+
+        list = sm.getSensorList(Sensor.TYPE_GYROSCOPE);
+        if(list.size()>0){
+            sm.registerListener(sel, (Sensor) list.get(0), SensorManager.SENSOR_DELAY_NORMAL);
+        }else{
+            Toast.makeText(getBaseContext(), "Error: No gyroscope.", Toast.LENGTH_LONG).show();
+        }
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        // data
+        series = new LineGraphSeries<DataPoint>();
+        graph.addSeries(series);
+        // customize a little bit viewport
+        Viewport viewport = graph.getViewport();
+        viewport.setYAxisBoundsManual(true);
+        viewport.setMinY(0);
+        viewport.setMaxY(10);
+        viewport.setScrollable(true);
+
+    }
+
+    @Override
+    protected void onStop() {
+        if(list.size()>0){
+            sm.unregisterListener(sel);
+        }
+        super.onStop();
+    }
+}
+
